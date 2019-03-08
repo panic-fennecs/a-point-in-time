d678cf181a6c93d1b06d6e46cd48f81a1a1ee39c
memoryleak47 <memoryleak47@protonmail.ch>
2019-03-08 19:52:01 +0100

    merged collision system

--- a_point_in_time/sprites/CollisionMap.gd
+++ a_point_in_time/sprites/CollisionMap.gd
@@ -3,3 +3,9 @@ extends Node2D
 func _ready():
 	var map = $"/root/Node2D/TileMap"
 	print(map.get_cell(0, 0))
+
+func has_collider_at(x, y):
+	var map = $"/root/Node2D/TileMap";
+	var id = map.get_cell(0, 0);
+	var ret = (x == 0 and y == 0) # TODO
+	return ret
