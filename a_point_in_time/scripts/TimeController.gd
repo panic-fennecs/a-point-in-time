8eb1bf893e65261ac0e6cbcb224cfeabaaad31a0
memoryleak47 <memoryleak47@protonmail.ch>
2019-03-09 14:18:04 +0100

    added Time Controller, you can now travel through time

---
+++ a_point_in_time/scripts/TimeController.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+enum TimeState { PRESENT, FUTURE }
+
+export var time_state = PRESENT
+
+const MANAGERS = ["BulbManager", "PlantManager"]
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func change_time(new_time_state):
+	time_state = new_time_state
+	
+	var m = []
+	for x in MANAGERS: m.append(get_node("/root/Node2D/" + x))
+	
+	if time_state == PRESENT:
+		for x in m: x.goto_present();
+	elif time_state == FUTURE:
+		for x in m: x.goto_future();
+	else:
+		assert(false)
--- a_point_in_time/scripts/TimeMachineTrigger.gd
+++ a_point_in_time/scripts/TimeMachineTrigger.gd
@@ -3,5 +3,16 @@ extends Node2D
 func _ready():
 	get_node("/root/Node2D/TriggerController").add_trigger(self);
 
+func other(t):
+	var c = get_node("/root/Node2D/TimeController")
+	if t == c.TimeState.FUTURE:
+		return c.TimeState.PRESENT
+	elif t == c.TimeState.PRESENT:
+		return c.TimeState.FUTURE
+	else:
+		assert(false)
+
 func trigger():
+	var c = get_node("/root/Node2D/TimeController")
+	c.change_time(other(c.time_state))
 	print("wupwup")
