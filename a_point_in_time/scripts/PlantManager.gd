82e6d0085bc23d7fc1beb4edbcacae24e37d355a
Bluemi <bruno.schilling@protonmail.ch>
2019-03-09 22:41:32 +0100

    implemented Plant logic

--- a_point_in_time/scripts/PlantManager.gd
+++ a_point_in_time/scripts/PlantManager.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var seed_scene = preload("res://scenes/Seed.tscn")
 var seed_item_scene = preload("res://scenes/SeedItem.tscn")
-var plant_scene = preload("res://scenes/Plant.tscn")
 
 """
 SEED_ON_TABLE_FUTURE:
@@ -23,115 +22,92 @@ enum PlantState {
 
 var plant_state = SEED_ON_TABLE_FUTURE
 
-# time changes:
+var POT_POSITION = null
+var SEED_TABLE_POSITION = null
+
+func _ready():
+	POT_POSITION = get_node("/root/Node2D/TriggerController/PotTrigger").position
+	SEED_TABLE_POSITION = get_node("/root/Node2D/TriggerController/SeedTableTrigger").position
 
 func goto_future():
-	if plant_state == SEED_IN_POT_FUTURE:
-		_add_seed()
-	elif plant_state == SEED_ON_TABLE_FUTURE:
-		_add_seed()
-	elif plant_state == SEED_IN_INVENTORY:
-		# seed stays in inventory, everything is fine :)
-		pass
-	elif plant_state == SEED_IN_POT_PRESENT:
-		_remove_seed()
-		_add_plant()
+	update()
 
 func goto_present():
-	if plant_state == SEED_IN_POT_FUTURE:
-		_remove_seed()
-	elif plant_state == SEED_ON_TABLE_FUTURE:
-		_remove_seed()
-	elif plant_state == SEED_IN_INVENTORY:
-		pass
-	elif plant_state == SEED_IN_POT_PRESENT:
-		_remove_plant()
-		_add_seed()
-
-func _remove_item():
-	get_child(0).queue_free()
-
-func _remove_seed():
-	get_child(0).queue_free()
-
-func _remove_plant():
-	get_child(0).queue_free()
+	update()
 
 func _add_item():
-	var seed_item = seed_scene.instance()
+	var seed_item = seed_item_scene.instance()
 	add_child(seed_item)
 
-func _add_seed():
+func _add_seed(pos):
 	var seed_obj = seed_scene.instance()
+	seed_obj.position = pos
 	add_child(seed_obj)
-
-func _add_plant():
-	var plant_obj = plant_scene.instance()
-	add_child(plant_obj)
-
-# trigger calls:
-
-func plant_seed_in_present():
-	assert plant_state == SEED_IN_INVENTORY
-	print("planting seed in present")
-	_remove_item()
-	_add_seed()
-	
-	plant_state = SEED_IN_POT_PRESENT
-
-func take_seed_in_present():
-	assert plant_state == SEED_IN_POT_PRESENT
-	print("taking seed in present")
-	_remove_seed()
-	_add_item()
-	
-	plant_state = SEED_IN_INVENTORY
-
-func take_seed_in_future():
-	assert plant_state == SEED_IN_POT_FUTURE or plant_state == SEED_ON_TABLE_FUTURE
-	print("taking seed in future")
-	_remove_seed()
-	_add_item()
-	
-	plant_state = SEED_IN_INVENTORY
-
-func plant_seed_in_future():
-	assert plant_state == SEED_IN_INVENTORY
-	print("planting seed in future")
-	_remove_item()
-	_add_seed()
-	
-	plant_state = SEED_IN_POT_FUTURE
-
-func touch_plant_in_future():
-	# TODO: Move to UI
-	print("This is a pretty flower :)")
 
 func on_touch_table():
 	var fut = get_node("/root/Node2D/TimeController").is_future()
 	if fut and plant_state == SEED_ON_TABLE_FUTURE:
 		print("noice, a seed")
-		take_seed_in_future()
+		plant_state = SEED_IN_INVENTORY
 	else:
 		print("wow, an empty table")
+
+	update()
 
 func on_touch_pot():
 	var fut = get_node("/root/Node2D/TimeController").is_future()
 	if fut:
 		if plant_state == SEED_IN_POT_FUTURE:
-			take_seed_in_future()
+			plant_state = SEED_IN_INVENTORY
 		elif plant_state == SEED_IN_INVENTORY:
-			plant_seed_in_future()
+			plant_state = SEED_IN_POT_FUTURE
 		elif plant_state == SEED_IN_POT_PRESENT:
-			touch_plant_in_future()
+			print("This is a pretty flower :)")
 		elif plant_state == SEED_ON_TABLE_FUTURE:
 			print("wow, an empty pot")
 	else:
 		if plant_state == SEED_IN_POT_FUTURE:
 			print("you can't do nothing, boii")
 		elif plant_state == SEED_IN_INVENTORY:
-			plant_seed_in_present()
+			plant_state = SEED_IN_POT_PRESENT
 		elif plant_state == SEED_IN_POT_PRESENT:
-			take_seed_in_present()
+			plant_state = SEED_IN_INVENTORY
 		elif plant_state == SEED_ON_TABLE_FUTURE:
 			print("wow, an empty pot")
+
+	update()
+
+func reset():
+	var seed_node = get_node("./Seed")
+	if seed_node:
+		seed_node.queue_free()
+
+	var item_node = get_node("./SeedItem")
+	if item_node:
+		item_node.queue_free()
+
+func update():
+	reset()
+
+	var fut = get_node("/root/Node2D/TimeController").is_future()
+	if fut:
+		if plant_state == SEED_IN_POT_FUTURE:
+			get_node("./Plant/AnimatedSprite").play("seed")
+		elif plant_state == SEED_IN_INVENTORY:
+			_add_item()
+			get_node("./Plant/AnimatedSprite").play("empty")
+		elif plant_state == SEED_IN_POT_PRESENT:
+			get_node("./Plant/AnimatedSprite").play("plant")
+		elif plant_state == SEED_ON_TABLE_FUTURE:
+			get_node("./Plant/AnimatedSprite").play("empty")
+			_add_seed(SEED_TABLE_POSITION)
+	else:
+		if plant_state == SEED_IN_POT_FUTURE:
+			get_node("./Plant/AnimatedSprite").play("empty")
+		elif plant_state == SEED_IN_INVENTORY:
+			get_node("./Plant/AnimatedSprite").play("empty")
+			_add_item()
+		elif plant_state == SEED_IN_POT_PRESENT:
+			get_node("./Plant/AnimatedSprite").play("seed")
+		elif plant_state == SEED_ON_TABLE_FUTURE:
+			get_node("./Plant/AnimatedSprite").play("empty")
