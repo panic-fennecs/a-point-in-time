1730f6722cfb5800c4fcd79614e9da48482ffb52
memoryleak47 <memoryleak47@protonmail.ch>
2019-03-10 20:34:59 +0100

    dankness prevails

--- a_point_in_time/scripts/BulbManager.gd
+++ a_point_in_time/scripts/BulbManager.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 var bulb_item_scene = preload("res://scenes/BulbItem.tscn")
 var lamp_obj_scene = preload("res://scenes/Lamp.tscn")
+var dankness_scene = preload("res://scenes/Dankness.tscn")
+
 var bulb_state = PRESENT_LAMP
 var dialog_controller
+var dankness_tiles = []
 
 """
 PRESENT_LAMP:
@@ -25,14 +28,25 @@ func _ready():
 	var lamp_obj = lamp_obj_scene.instance()
 	add_child(lamp_obj)
 	dialog_controller = get_node("/root/Node2D/PlayerCamera/DialogCanvas")
+	for x in range(4, 9):
+		for y in range(12, 18):
+			if [7, 8].has(x) and [12, 13].has(y):
+				continue
+			var t = dankness_scene.instance()
+			t.get_child(0).play("default")
+			t.position.x = x * 64 + 32
+			t.position.y = y * 64 + 32
+			add_child(t)
+			t.hide()
+			dankness_tiles.append(t)
 
 func _lamp_off():
 	get_node("./Lamp/AnimatedSprite").play("off")
-	get_node("./Darkness").show()
+	for t in dankness_tiles: t.show()
 
 func _lamp_on():
 	get_node("./Lamp/AnimatedSprite").play("on")
-	get_node("./Darkness").hide()
+	for t in dankness_tiles: t.hide()
 
 func goto_future():
 	if bulb_state == PRESENT_LAMP:
