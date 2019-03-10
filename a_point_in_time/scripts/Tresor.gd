f8cf6d79ba63bca8ec267dceab2a9a0a96939fb9
memoryleak47 <memoryleak47@protonmail.ch>
2019-03-10 12:47:06 +0100

    made Tresor a manager

--- a_point_in_time/scripts/Tresor.gd
+++ a_point_in_time/scripts/Tresor.gd
@@ -1,8 +1,23 @@
 
 extends Node2D
 
+enum TresorNoteState {
+	START,
+	NOTE_IN_INV,
+	OPENED,
+}
+
+var tresor_note_state = START
+
 func _ready():
 	get_node("/root/Node2D/TriggerController").add_trigger(self);
 
 func trigger():
 	print("tresor und so")
+
+func goto_future():
+	$AnimatedSprite.play("open")
+
+func goto_present():
+	if tresor_note_state != OPENEND:
+		$AnimatedSprite.play("closed")
