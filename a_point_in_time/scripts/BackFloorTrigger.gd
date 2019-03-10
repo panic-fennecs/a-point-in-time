92342b920f793ad40bb6bc5bfaded69de278fc71
memoryleak47 <memoryleak47@protonmail.ch>
2019-03-10 03:53:43 +0100

    the dark room is not dark, when there is a lamp now.. obviously

--- a_point_in_time/scripts/BackFloorTrigger.gd
+++ a_point_in_time/scripts/BackFloorTrigger.gd
@@ -6,10 +6,12 @@ func _ready():
 	get_node("/root/Node2D/TriggerController").add_floor_trigger(self);
 
 func floor_trigger():
-	var player = get_node("/root/Node2D/Player")
-	var dialog_controller = get_node("/root/Node2D/PlayerCamera/DialogCanvas")
-	if is_X:
-		player.move_left()
-	else:
-		player.move_bot()
-	dialog_controller.show_dialog("dark")
+	var lamp_sprite = get_node("/root/Node2D/BulbManager/Lamp/AnimatedSprite")
+	if lamp_sprite.animation == "off":
+		var player = get_node("/root/Node2D/Player")
+		var dialog_controller = get_node("/root/Node2D/PlayerCamera/DialogCanvas")
+		if is_X:
+			player.move_left()
+		else:
+			player.move_bot()
+		dialog_controller.show_dialog("dark")
